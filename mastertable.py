c1113e8f6885c53385f43b3055bdec0769151007
Yung-Lung Chang <ylchang@mail.ndmctsgh.edu.tw>
2022-02-17 16:07:16 +0800

    Finish test sheet generation

--- mastertable.py
+++ mastertable.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
+import shutil
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
-from reportlab.lib.units import cm, inch
+from reportlab.lib.units import mm, cm, inch
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from io import BytesIO
 import streamlit as st
 import os
 import base64
 from PIL import Image
 from batch_fill_AS import batchFillAS
-
+from PyPDF2 import PdfFileWriter, PdfFileReader
 # ------------------------------------------
 
 pdfmetrics.registerFont(TTFont('Microsoft Jhenghei', 'Microsoft Jhenghei.ttf'))
@@ -105,6 +107,7 @@ def columnize(data, rows, cols, heading=0):
 #     b64 = base64.b64encode(val)  # val looks like b'...'
 #     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
+
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
@@ -118,10 +121,57 @@ def to_excel(df):
     return processed_data
 
 
+def fillTestSheet(uploaded_file, preview, page_num_to_trim, ID_LEFT, ID_HEIGHT, NAME_LEFT, NAME_HEIGHT, CLASS_LEFT, CLASS_HEIGHT, SEAT_LEFT, SEAT_HEIGHT):
+    packet = BytesIO()
+    # create a new PDF with Reportlab
+    can = canvas.Canvas(packet, pagesize=A4)
+    can.setFont('Microsoft Jhenghei', 12)
+    can.drawString(ID_LEFT * mm, ID_HEIGHT * mm, str(ID))
+    can.drawString(NAME_LEFT * mm, NAME_HEIGHT * mm, Name)
+    can.drawString(CLASS_LEFT * mm, CLASS_HEIGHT * mm, Class)
+    can.drawString(SEAT_LEFT * mm, SEAT_HEIGHT * mm, f'Seat：{Seat}')
+    can.save()
+    # move to the beginning of the StringIO buffer
+    packet.seek(0)
+    new_pdf = PdfFileReader(packet)
+    # read your existing PDF
+    # bytes_data = file.read()
+    # print(bytes_data)
+    existing_pdf = PdfFileReader(BytesIO(uploaded_file.getvalue()), "rb")
+    output = PdfFileWriter()
+    # add the "watermark" (which is the new pdf) on the existing page
+    page = existing_pdf.getPage(0)
+    page.mergePage(new_pdf.getPage(0))
+    output.addPage(page)
+
+    for pageNum in range(1, existing_pdf.numPages - page_num_to_trim):
+        page_obj = existing_pdf.getPage(pageNum)
+        output.addPage(page_obj)
+
+    # finally, write "output" to a real file
+    if preview:
+        output_stream = open(f'preview.pdf', "wb")
+    else:
+        output_stream = open(f'./tmp/{Seat}.pdf', "wb")
+    output.write(output_stream)
+    output_stream.close()
+
+
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # Embedding PDF in HTML
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
 
 menu = ["試場座位", "答案卡", "試題卷", "匯整正確答案", "成績計算"]
 choice = st.sidebar.selectbox('Menu', menu)
 st.sidebar.write("---")
+
+
 if choice == "試場座位":
     st.title("試務工作流程-步驟一")
     st.write("## 製作試場座位表")
@@ -172,23 +222,62 @@ if choice == "答案卡":
     ID_top = st.sidebar.number_input("答案卡下緣至學號上邊界之距離: ", 192.58)
     ID_bottom = st.sidebar.number_input("答案卡下緣至學號下邊界之距離:: ", 151.05)
     if uploaded_mt is not None:
-        df_seat = pd.read_excel(uploaded_mt)
+        df_seat = pd.read_excel(uploaded_mt, index_col=0)
         st.dataframe(df_seat)
-        # ID_LEFT_MARGIN = 20.45
-        # ID_RIGHT_MARGIN = 60.47
-        # ID_TOP_MARGIN = 192.58
-        # ID_BOTTOM_MARGIN = 151.05
         batchFillAS(df_seat, "AnswerSheet.pdf", as_title, ID_left, ID_right, ID_top, ID_bottom)
         with open("AnswerSheet.pdf", 'rb') as pdf_file:
             PDFbyte = pdf_file.read()
         st.download_button('Download PDF', data=PDFbyte, file_name="AnswerSheet.pdf", mime="application/octet-stream")
-
-
-
 if choice == "試題卷":
-    st.write("施工中")
-    image = Image.open("under_construction.gif")
-    st.image(image)
+    st.sidebar.subheader("1. 上傳 masterTable.xlsx")
+    uploaded_mt = st.sidebar.file_uploader("檔案格式: xlsx", key = 2)
+    st.sidebar.subheader("2. 上傳試卷pdf")
+    uploaded_files = st.sidebar.file_uploader("Upload PDF file(s)", accept_multiple_files=True, key=3)
+    if uploaded_mt is not None:
+        df = pd.read_excel(uploaded_mt, index_col=0)
+        st.dataframe(df)
+        df_array = df.sort_values(by='Seat_index').to_numpy()
+        num_rows, num_cols = df_array.shape
+    if uploaded_files and uploaded_mt is not None:
+        with st.expander("定位參數修改"):
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                ID_LEFT = st.number_input("頁面左邊界至學號左緣之距離(mm): ", value=30)
+                ID_HEIGHT = st.number_input("頁面下邊界至學號下緣之距離(mm): ", value=281)
+                page_num_to_trim = st.number_input("刪減PDF檔倒數頁數: ", value=2)
+            with col2:
+                NAME_LEFT = st.number_input("頁面左邊界至姓名左緣之距離(mm): ", value=65)
+                NAME_HEIGHT = st.number_input("頁面下邊界至姓名下緣之距離(mm): ", value=281)
+            with col3:
+                CLASS_LEFT = st.number_input("頁面左邊界至期班左緣之距離(mm): ", value=105)
+                CLASS_HEIGHT = st.number_input("頁面下邊界至期班下緣之距離(mm): ", value=281)
+            with col4:
+                SEAT_LEFT = st.number_input("頁面左邊界至座位左緣之距離(mm): ", value=167)
+                SEAT_HEIGHT = st.number_input("頁面下邊界至座位下緣之距離(mm): ", value=287)
+        if st.button("Preview"):
+            ID, Name, Class, Seat_index, Seat, Version = df_array[0,:]
+            fillTestSheet(uploaded_files[0],True, page_num_to_trim, ID_LEFT, ID_HEIGHT, NAME_LEFT, NAME_HEIGHT, CLASS_LEFT, CLASS_HEIGHT, SEAT_LEFT, SEAT_HEIGHT)
+            displayPDF("preview.pdf")
+        if st.button("Make All Sheets"):
+            progress_bar = st.progress(0)
+            current_progress = 0.0
+            for ID, Name, Class, Seat_index, Seat, Version in df_array:
+                for file in uploaded_files:
+                    if 'Ver_' + str(Version) in file.name:
+                        fillTestSheet(file, False, page_num_to_trim, ID_LEFT, ID_HEIGHT, NAME_LEFT, NAME_HEIGHT, CLASS_LEFT, CLASS_HEIGHT, SEAT_LEFT, SEAT_HEIGHT)
+                        current_progress += (1/(num_rows))
+                        progress_bar.progress(current_progress)
+            shutil.make_archive("archive", 'zip', "./tmp")
+            progress_bar.progress(1.0)
+            st.success("Done!")
+            st.balloons()
+            with open("archive.zip", "rb") as zf:
+                btn = st.download_button(
+                label='Download ZIP',
+                data=zf,
+                file_name="archive.zip",
+                mime="application/zip")
+
 if choice == "匯整正確答案":
     st.write("施工中")
     image = Image.open("under_construction.gif")
