2eb0014d8bb9be7884c53ab639b2f2397b973d66
Yung-Lung Chang <ylchang@mail.ndmctsgh.edu.tw>
2022-02-17 16:24:01 +0800

    f to F

--- mastertable.py
+++ mastertable.py
@@ -162,7 +162,7 @@ def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     # Embedding PDF in HTML
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
