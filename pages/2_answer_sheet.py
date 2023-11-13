56f0ccb6f19689aed88210b13c2bc239914a7163
Yung-Lung Chang <ylchang@mail.ndmctsgh.edu.tw>
2023-11-13 16:56:30 +0800

    remove default test name

--- pages/2_answer_sheet.py
+++ pages/2_answer_sheet.py
@@ -3,7 +3,7 @@ import pandas as pd
 from batch_fill_AS import batchFillAS
 
 st.sidebar.subheader("1. 輸入考試名稱")
-as_title = st.sidebar.text_input('印於答案卡之標題', "Biochem-1")
+as_title = st.sidebar.text_input('印於答案卡之標題')
 st.sidebar.subheader("2. 上傳 masterTable.xlsx")
 uploaded_mt = st.sidebar.file_uploader("檔案格式: xlsx", key=2)
 st.sidebar.subheader("3.設定學號列印位置，單位 mm")
@@ -18,15 +18,19 @@ if uploaded_mt is None:
     2. 確認列印選項中的縮放比例無調整(100%)
     3. 先列印一張，調整參數確認無誤後再印出全部
     '''
+
 else:
-    df_seat = pd.read_excel(uploaded_mt, index_col=0)
-    st.dataframe(df_seat)
-    batchFillAS(df_seat, "AnswerSheet.pdf", as_title,
-                ID_left, ID_right, ID_top, ID_bottom)
-    with open("AnswerSheet.pdf", 'rb') as pdf_file:
-        PDFbyte = pdf_file.read()
-    st.download_button(
-        'Download PDF',
-        data=PDFbyte,
-        file_name="AnswerSheet.pdf",
-        mime="application/octet-stream")
+    if as_title == "":
+        st.warning("記得輸入考試標題")
+    else:
+        df_seat = pd.read_excel(uploaded_mt, index_col=0)
+        st.dataframe(df_seat)
+        batchFillAS(df_seat, "AnswerSheet.pdf", as_title,
+                    ID_left, ID_right, ID_top, ID_bottom)
+        with open("AnswerSheet.pdf", 'rb') as pdf_file:
+            PDFbyte = pdf_file.read()
+        st.download_button(
+            'Download PDF',
+            data=PDFbyte,
+            file_name="AnswerSheet.pdf",
+            mime="application/octet-stream")
